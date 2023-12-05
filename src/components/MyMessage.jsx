14a20b42929538721cc0de7b9cd0520170e1b0d8
kavinda Methsara <methsara31@gmail.com>
2023-12-05 20:20:09 +0530

    Created the code for their message

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -5,14 +5,15 @@ const MyMessage = ({ message }) => {
                 src={message.attachments[0].file}
                 alt="message-attachments"
                 className="message-image"
-                style={{float: 'right'}}
+                style={{ float: 'right' }}
             />
         )
+    }
     return (
-        <div className="message" style={{float:'right', marginRight:'18px', color:'white', backgroundColor:'#3B2A50'}}>
+        <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
             {message.text}
         </div>
-    )
+    );
 }
 
 export default MyMessage;
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,7 +1,35 @@
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+  const isFirstMessageByUser =
+    !lastMessage || lastMessage.sender.username != message.sender.username;
   return (
-    <div>TheirMessage</div>
-  )
-}
+    <div className="message-row">
+      {isFirstMessageByUser && (
+        <div
+          className="message-avatar"
+          style={{ backgroundImage: `url(${message?.sender?.username})` }}
+        />
+      )}
+      {message?.attachments?.length > 0 ? (
+        <img
+          src={message.attachments[0].file}
+          alt="message-attachments"
+          className="message-image"
+          style={{ marginleft: isFirstMessageByUser ? "4px" : "48px" }}
+        />
+      ) : (
+        <div
+          className="message"
+          style={{
+            float: "left",
+            backgroundColor: "#CABCDC",
+            marginleft: isFirstMessageByUser ? "4px" : "48px",
+          }}
+        >
+          {message.text}
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default TheirMessage
+export default TheirMessage;
